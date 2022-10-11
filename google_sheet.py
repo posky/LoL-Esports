a7cff4488fdacb9a9d3160b036ba3f504384449d
posky <pjm5810@gmail.com>
2022-10-11 19:31:29 +0900

    google sheet api

--- google_sheet.py
+++ google_sheet.py
@@ -2,12 +2,14 @@ from __future__ import print_function
 
 import os.path
 from pprint import pprint
+import numpy as np
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
@@ -65,6 +67,47 @@ class Sheet:
             response = request.execute()
         except HttpError as err:
             print(err)
+
+    def update_sheet(self, sheet_name, df, index=True, value_input_option='USER_ENTERED'):
+        if index:
+            values = [list(df.index.names) + df.columns.tolist()]
+            if len(df.index.names) > 1:
+                values += [
+                    list(_index) + value for _index, value in zip(
+                        df.index.tolist(), df.replace(
+                            [np.inf, -np.inf], np.nan
+                        ).fillna('').values.tolist()
+                    )
+                ]
+            else:
+                values += [
+                    [_index] + value for _index, value in zip(
+                        df.index.tolist(), df.replace(
+                            [np.inf, -np.inf], np.nan
+                        ).fillna('').values.tolist()
+                    )
+                ]
+        else:
+            values = [df.columns.tolist()]
+            values += [
+                value for value in df.replace(
+                    [np.inf, -np.inf], np.nan
+                ).fillna('').values.tolist()
+            ]
+
+        num_cols = len(values[0])
+        sheet_range = f'{sheet_name}!R1C1:C{num_cols}'
+        rows = self.read_rows_sheet(sheet_range)
+        self.write_sheet(
+            sheet_range,
+            value_input_option,
+            {'values': [['' for _ in range(num_cols)] for _ in range(len(rows))]}
+        )
+        self.write_sheet(
+            sheet_range,
+            value_input_option,
+            {'values': values}
+        )
 
 
 def main():
