09c663ff1469d0f54b59134f27c0019c0bbe7c31
posky <pjm5810@gmail.com>
2022-10-18 21:25:11 +0900

    google sheets api

--- google_sheet.py
+++ google_sheet.py
@@ -29,7 +29,14 @@ class Sheet:
         # If there are no (valid) credentials available, let the user log in.
         if not creds or not creds.valid:
             if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
+                try:
+                    creds.refresh(Request())
+                except Exception as e:
+                    print(e)
+                    flow = InstalledAppFlow.from_client_secrets_file(
+                        'credentials.json', SCOPES
+                    )
+                    creds = flow.run_local_server(port=0)
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
                     'credentials.json', SCOPES)
