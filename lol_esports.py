30aa81193baa4badf0c62e6f1f6f90f552fc1ce0
posky <pjm5810@gmail.com>
2023-05-26 13:20:49 +0900

    lolesports league simulation

--- lol_esports.py
+++ lol_esports.py
@@ -1,200 +1,362 @@
-MATCH_BO3 = [(2, 0,), (2, 1,), (1, 2,), (0, 2,)]
+from itertools import product
 
-# Match class
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+
+from lol_fandom import get_tournaments, get_match_schedule
+
+
+class Head2Head:
+    def __init__(self):
+        self.win = 0
+        self.loss = 0
+        self.set_win = 0
+        self.set_loss = 0
+
+    def __str__(self):
+        return f"({self.win}, {self.loss})"
+
+
 class Match:
-    def __init__(self, team1, team2, winner, team1_score, team2_score, num):
+    BEST_OF = {3: [(2, 0), (2, 1), (1, 2), (0, 2)]}
+
+    def __init__(
+        self, team1, team2, winner, team1_score, team2_score, is_tiebreaker, best_of
+    ):
         self.team1 = team1
         self.team2 = team2
         self.winner = winner
         self.team1_score = team1_score
         self.team2_score = team2_score
-        self.num = num
+        self.is_tiebreaker = is_tiebreaker
+        self.best_of = best_of
 
-    def set_seq(self, seq):
-        team1_score, team2_score = MATCH_BO3[seq]
-        self.team1_score = team1_score
-        self.team2_score = team2_score
-        self.winner = seq // 2 + 1
+    def set_match(self, idx):
+        self.team1_score = self.BEST_OF[self.best_of][idx][0]
+        self.team2_score = self.BEST_OF[self.best_of][idx][1]
+        if self.team1_score > self.team2_score:
+            self.winner = 1
+        else:
+            self.winner = 2
 
-    def init_seq(self):
-        self.winner = -1
+    def init_match(self):
         self.team1_score = 0
         self.team2_score = 0
+        self.winner = 0
 
-# Team class
+    def __str__(self):
+        return f"{self.team1} {self.team1_score} vs {self.team2_score} {self.team2}"
+
+
 class Team:
-    def __init__(self, name, league):
+    def __init__(self, name):
         self.name = name
-        self.league = league
         self.win = 0
         self.loss = 0
         self.set_win = 0
         self.set_loss = 0
+        self.streak = 0
+        self.standing = 0
         self.matches = []
+        self.proceeded_matches = []
+        self.head_to_head = {}
+
+    @property
+    def games(self):
+        return self.win + self.loss
+
+    @property
+    def set_games(self):
+        return self.set_win + self.set_loss
 
     @property
     def point(self):
         return self.set_win - self.set_loss
 
     @property
-    def streak(self):
-        #
-        return 0
+    def winrate(self):
+        return self.win / self.games if self.win > 0 else 0
 
-    def _match_result(self, match):
-        if match.winner == 1:
-            if match.team1 == self.name:
-                self.win += 1
-                self.set_win += match.team1_score
-                self.set_loss += match.team2_score
-            else:
-                self.loss += 1
-                self.set_win += match.team2_score
-                self.set_loss += match.team1_score
-        elif match.winner == 2:
-            if match.team1 == self.name:
-                self.loss += 1
-                self.set_win += match.team1_score
-                self.set_loss += match.team2_score
-            else:
-                self.win += 1
-                self.set_win += match.team2_score
-                self.set_loss += match.team1_score
+    @property
+    def set_winrate(self):
+        return self.set_win / self.set_games if self.set_win > 0 else 0
+
+    def init_head_to_head(self, team_names):
+        for name in team_names:
+            if name != self.name:
+                self.head_to_head[name] = Head2Head()
 
     def add_match(self, match):
         self.matches.append(match)
-        self._match_result(match)
 
-    def set_match(self, match):
-        self._match_result(match)
+    def _update_match(self, match):
+        if self.name == match.team1:
+            if match.winner == 1:
+                self.win += 1
+            else:
+                self.loss += 1
+            self.set_win += match.team1_score
+            self.set_loss += match.team2_score
+        else:
+            if match.winner == 1:
+                self.loss += 1
+            else:
+                self.win += 1
+            self.set_win += match.team2_score
+            self.set_loss += match.team1_score
 
-    def undo_match(self, match):
-        if match.winner == 1:
-            if match.team1 == self.name:
+    def _rollback_match(self, match):
+        if self.name == match.team1:
+            if match.winner == 1:
                 self.win -= 1
-                self.set_win -= match.team1_score
-                self.set_loss -= match.team2_score
             else:
                 self.loss -= 1
-                self.set_win -= match.team2_score
-                self.set_loss -= match.team1_score
-        elif match.winner == 2:
-            if match.team1 == self.name:
+            self.set_win -= match.team1_score
+            self.set_loss -= match.team2_score
+        else:
+            if match.winner == 1:
                 self.loss -= 1
-                self.set_win -= match.team1_score
-                self.set_loss -= match.team2_score
             else:
                 self.win -= 1
-                self.set_win -= match.team2_score
-                self.set_loss -= match.team1_score
+            self.set_win -= match.team2_score
+            self.set_loss -= match.team1_score
+
+    def update_head_to_head(self, match):
+        if match.team1 == self.name:
+            if match.winner == 1:
+                self.head_to_head[match.team2].win += 1
+            else:
+                self.head_to_head[match.team2].loss += 1
+            self.head_to_head[match.team2].set_win += match.team1_score
+            self.head_to_head[match.team2].set_loss += match.team2_score
+        else:
+            if match.winner == 2:
+                self.head_to_head[match.team1].win += 1
+            else:
+                self.head_to_head[match.team1].loss += 1
+            self.head_to_head[match.team1].set_win += match.team2_score
+            self.head_to_head[match.team1].set_loss += match.team1_score
+
+    def rollback_head_to_head(self, match):
+        if match.team1 == self.name:
+            if match.winner == 1:
+                self.head_to_head[match.team2].win -= 1
+            else:
+                self.head_to_head[match.team2].loss -= 1
+            self.head_to_head[match.team2].set_win -= match.team1_score
+            self.head_to_head[match.team2].set_loss -= match.team2_score
+        else:
+            if match.winner == 2:
+                self.head_to_head[match.team1].win -= 1
+            else:
+                self.head_to_head[match.team1].loss -= 1
+            self.head_to_head[match.team1].set_win -= match.team2_score
+            self.head_to_head[match.team1].set_loss -= match.team1_score
+
+    def update_match(self, match):
+        self.proceeded_matches.append(match)
+        self._update_match(match)
+        self.update_head_to_head(match)
+
+    def update_rest_match(self, match):
+        self._update_match(match)
+        self.update_head_to_head(match)
+
+    def rollback_rest_match(self, match):
+        self._rollback_match(match)
+        self.rollback_head_to_head(match)
+
+    def __str__(self):
+        return f"{self.name}: {self.games} | {self.win} - {self.loss} | {self.set_win} - {self.set_loss} | {self.point}"
 
     def __lt__(self, other):
         if self.win != other.win:
-            return self.win < other.win
+            return self.win > other.win
         if self.loss != other.loss:
-            return self.loss > other.loss
-        return self.point < other.point
-
-    def __eq__(self, other):
-        if self.win == other.win and self.loss == other.loss and self.point == other.point:
-            return True
+            return self.loss < other.loss
+        if self.point != other.point:
+            return self.point > other.point
         return False
 
+    def __eq__(self, other):
+        if self.win != other.win:
+            return False
+        if self.loss != other.loss:
+            return False
+        if self.set_win != other.set_win:
+            return False
+        if self.set_loss != other.set_loss:
+            return False
+        return True
 
 
-# League class
 class League:
     def __init__(self, name):
         self.name = name
         self.teams = {}
         self.matches = []
+        self.proceeded_matches = []
+        self.rest_matches = []
 
-    def add_team(self, team_name):
-        if team_name not in self.teams:
-            self.teams[team_name] = Team(team_name, self)
+    def add_team(self, name):
+        self.teams[name] = Team(name)
 
-    def add_teams(self, team_names):
-        for name in team_names:
-            self.add_team(name)
+    def add_match(self, match):
+        assert isinstance(match, Match)
 
-    def add_match(self, match_info):
-        match = Match(
-            match_info['team1'],
-            match_info['team2'],
-            match_info['winner'],
-            match_info['team1_score'],
-            match_info['team2_score'],
-            match_info['num']
-        )
         self.matches.append(match)
         self.teams[match.team1].add_match(match)
         self.teams[match.team2].add_match(match)
 
-    def add_matches(self, matches):
-        for match in matches:
-            self.add_match(match)
+    def update_match(self, match):
+        self.proceeded_matches.append(match)
+        self.teams[match.team1].update_match(match)
+        self.teams[match.team2].update_match(match)
 
-    def set_match(self, match, seq):
-        match.set_seq(seq)
-        self.teams[match.team1].set_match(match)
-        self.teams[match.team2].set_match(match)
+    def update_rest_match(self, match):
+        self.teams[match.team1].update_rest_match(match)
+        self.teams[match.team2].update_rest_match(match)
 
-    def undo_match(self, match):
-        if match.winner != -1:
-            self.teams[match.team1].undo_match(match)
-            self.teams[match.team2].undo_match(match)
-            match.init_seq()
+    def rollback_rest_match(self, match):
+        self.teams[match.team1].rollback_rest_match(match)
+        self.teams[match.team2].rollback_rest_match(match)
+
+    def init_head_to_head(self):
+        for team in self.teams.values():
+            team.init_head_to_head(self.teams.keys())
+
+    def proceed_matches(self):
+        for match in self.matches:
+            if match.winner != 0:
+                self.update_match(match)
+            else:
+                self.rest_matches.append(match)
 
     def rank(self):
-        standings = sorted(self.teams.values(), reverse=True)
+        standings = sorted(self.teams.values())
+        for i, team in enumerate(standings, start=1):
+            team.standing = i
         return standings
 
-    def simulate(self):
-        standings = {}
-        for team in self.teams:
-            standings[team] = [0 for _ in range(len(self.teams) + 1)]
+    def tiebreak(self, teams):
+        if len(teams) == 2:
+            head_to_head = teams[0].head_to_head[teams[1].name]
+            if head_to_head.win > head_to_head.loss:
+                teams[1].standing = teams[0].standing + 1
+            elif head_to_head.win < head_to_head.loss:
+                teams[0].standing = teams[1].standing + 1
 
-        rest_matches = []
-        for match in self.matches:
-            if match.winner == -1:
-                rest_matches.append(match)
+            print(f"{teams[0].name} vs {teams[1].name}: {head_to_head}")
 
-        full_case = pow(4, len(rest_matches))
-        cur_case = 0
+        elif len(teams) == 3:
+            head_to_head_lst = [Head2Head() for _ in range(3)]
+            for i, (team, head_to_head) in enumerate(zip(teams, head_to_head_lst)):
+                op_teams = teams[:i] + teams[i + 1 :]
+                head_to_head.win = (
+                    team.head_to_head[op_teams[0].name].win
+                    + team.head_to_head[op_teams[1].name].win
+                )
+                head_to_head.loss = (
+                    team.head_to_head[op_teams[0].name].loss
+                    + team.head_to_head[op_teams[1].name].loss
+                )
+            lst = sorted(
+                zip(teams, head_to_head_lst), key=lambda x: x[1].win, reverse=True
+            )
+            if lst[0][1].win == 3 and lst[1][1].win == 3 and lst[2][1].win == 0:
+                lst[2][0].standing = lst[0][0].standing + 2
+            elif lst[0][1].win == 4 and lst[1][1].win == 1 and lst[2][1].win == 1:
+                lst[1][0].standing = lst[0][0].standing + 1
+                lst[2][0].standing = lst[0][0].standing + 1
+            elif lst[0][1].win == 4 and lst[1][1].win == 2 and lst[2][1].win == 0:
+                lst[1][0].standing = lst[0][0].standing + 1
+                lst[2][0].standing = lst[0][0].standing + 2
 
-        stack = [(0, -1)] if len(rest_matches) > 0 else []  # (number of rest_matches, seq)
-        while len(stack) > 0:
-            i, seq = stack.pop()
-            match = rest_matches[i]
-            self.undo_match(match)
+            for team, head_to_head in lst:
+                print(f"{team.standing}: {team} | {head_to_head}")
+            print()
 
-            seq += 1
-            # print('{:.2f}%'.format(cur_case / full_case * 100), end='\r')
-            print(f'{cur_case / full_case * 100:.6f}% ({cur_case} / {full_case})', end='\r')
-            while seq < 4:
-                match = rest_matches[i]
-                self.set_match(match, seq)
-                stack.append((i, seq,))
-                if i == len(rest_matches) - 1:
-                    cur_case += 1
-                    standing = self.rank()
-                    pre_pos = 1
-                    pre_team = None
-                    for pos, team in enumerate(standing, start=1):
-                        if pos > 1 and pre_team == team:
-                            pos = pre_pos
-                        standings[team.name][pos] += 1
-                        pre_pos = pos
-                        pre_team = team
-                    break
-                i += 1
-                seq = 0
+    def simulate_rest_matches(self):
+        num = 4 ** len(self.rest_matches)
+        with tqdm(total=num) as pbar:
+            for idx in product("0123", repeat=len(self.rest_matches)):
+                for i, match in zip(idx, self.rest_matches):
+                    match.set_match(int(i))
+                    self.update_rest_match(match)
 
-        return standings
+                standings = self.rank()
+                lst = [standings[0]]
+                for i, team in enumerate(standings[1:], start=1):
+                    if standings[i - 1] == team:
+                        lst.append(team)
+                        team.standing = lst[0].standing
+                    elif len(lst) > 2:
+                        self.tiebreak(lst)
+                        lst = [team]
+                    else:
+                        lst = [team]
+                if len(lst) > 2:
+                    self.tiebreak(lst)
+
+                standings = sorted(standings, key=lambda x: x.standing)
+
+                for match in self.rest_matches:
+                    self.rollback_rest_match(match)
+                    match.init_match()
+                pbar.update(1)
+
+
+def update_matches_to_csv(page_name):
+    match_schedules = get_match_schedule(where=f'MS.OverviewPage="{page_name}"')
+    match_schedules.sort_values(by=["DateTime UTC"], ignore_index=True, inplace=True)
+    columns = [
+        "Team1",
+        "Team2",
+        "Winner",
+        "Team1Score",
+        "Team2Score",
+        "IsTiebreaker",
+        "BestOf",
+    ]
+    matches = match_schedules[columns].fillna(0)
+    columns = ["Winner", "Team1Score", "Team2Score", "IsTiebreaker", "BestOf"]
+    matches[columns] = matches[columns].astype("int")
+    matches.to_csv("./csv/match_schedule/target_matches_schedule.csv", index=False)
+
 
 def main():
-    pass
+    page = "LCK/2023 Season/Spring Season"
+    update_matches_to_csv(page)
+    matches = pd.read_csv("./csv/match_schedule/target_matches_schedule.csv")
+
+    league = League("LCK")
+    team_names = matches[["Team1", "Team2"]].unstack().unique()
+    for name in team_names:
+        league.add_team(name)
+
+    league.init_head_to_head()
+
+    for row in matches.itertuples():
+        match = Match(
+            row.Team1,
+            row.Team2,
+            row.Winner,
+            row.Team1Score,
+            row.Team2Score,
+            row.IsTiebreaker,
+            row.BestOf,
+        )
+        league.add_match(match)
+
+    league.proceed_matches()
+
+    standings = league.rank()
+    for team in standings:
+        print(team)
+
+    league.simulate_rest_matches()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
