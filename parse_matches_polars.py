9dca1ef765524ba212101bcaba983edacc19bb08
posky <pjm5810@gmail.com>
2023-10-02 22:40:27 +0900

    feat: parse scoreboard players

--- parse_matches_polars.py
+++ parse_matches_polars.py
@@ -6,7 +6,12 @@ from pathlib import Path
 import polars as pl
 from tqdm import tqdm
 
-from lol_fandom_pl import get_leagues, get_scoreboard_games, get_tournaments
+from lol_fandom_pl import (
+    get_leagues,
+    get_scoreboard_games,
+    get_scoreboard_players,
+    get_tournaments,
+)
 
 
 def get_updated_contents(df: pl.DataFrame, path: Path) -> pl.DataFrame:
@@ -130,11 +135,72 @@ def parse_scoreboard_games(start: int = 2011, end: int | None = None) -> None:
         logging.debug("%d tournaments %s", year, tournaments.shape)
 
 
+def parse_scoreboard_players(start: int = 2011, end: int | None = None) -> None:
+    """Parses the scoreboard players data for a given range of years.
+
+    Args:
+        start (int, optional): The starting year to parse. Defaults to 2011.
+        end (int | None, optional): The ending year to parse.
+            If None, the current year is used. Defaults to None.
+
+    Returns:
+        None
+    """
+    if end is None:
+        end = datetime.datetime.now(tz=datetime.UTC).year
+
+    for year in tqdm(range(start, end + 1), desc="Year"):
+        tournaments = pl.read_parquet(f"./csv/tournaments/{year}_tournaments.parquet")
+        scoreboard_games = pl.read_parquet(
+            f"./csv/scoreboard_games/{year}_scoreboard_games.parquet",
+        )
+        logging.debug("%d - tournament %s", year, tournaments.shape)
+        logging.debug("%d - scoreboard games %s", year, scoreboard_games.shape)
+        scoreboard_players = pl.DataFrame()
+        for (page,) in tqdm(
+            tournaments.select(pl.col("OverviewPage")).rows(),
+            desc="Scoreboard Players",
+        ):
+            logging.debug("\t%s", page)
+            teams = (
+                scoreboard_games.filter(pl.col("OverviewPage") == page)
+                .melt(id_vars="OverviewPage", value_vars=["Team1", "Team2"])
+                .select(pl.col("value"))
+                .unique()
+                .to_series()
+                .to_list()
+            )
+            len_sp = 0
+            for team in tqdm(teams, deps="Teams"):
+                sp = get_scoreboard_players(
+                    where=f'T.OverviewPage="{page}" and SP.Team="{team}"',
+                )
+                if sp.shape[0] > 0:
+                    len_sp += sp.shape[0]
+                    scoreboard_players = pl.concat([scoreboard_players, sp])
+            len_sg = scoreboard_games.filter(pl.col("OverviewPage") == page).shape[0]
+            logging.debug(
+                "\t\tscoreboard games - %d | scoreboard players - %d | %s",
+                len_sg,
+                len_sp,
+                len_sg * 10 == len_sp,
+            )
+        scoreboard_players = scoreboard_players.sort(
+            by=["DateTime UTC", "Team", "Role Number"],
+        )
+        file_path = Path(f"./csv/scoreboard_players/{year}_scoreboard_players.parquet")
+        if not file_path.parent.is_dir():
+            Path.mkdir(file_path.parent, parents=True)
+        scoreboard_players.write_parquet(file_path)
+        logging.debug("%d scoreboard players %s", year, scoreboard_players.shape)
+
+
 def main() -> None:
     logging.basicConfig(level=logging.INFO)
 
-    parse_tournaments(start=2011)
-    parse_scoreboard_games(start=2011)
+    parse_tournaments(start=2023)
+    parse_scoreboard_games(start=2023)
+    parse_scoreboard_players(start=2023)
 
 
 if __name__ == "__main__":
