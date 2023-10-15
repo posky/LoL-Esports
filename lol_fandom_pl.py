2f25569658c7316724395e41101d333a0f0c7c3f
posky <pjm5810@gmail.com>
2023-10-15 20:53:40 +0900

    feat: add get scoreboard games

--- lol_fandom_pl.py
+++ lol_fandom_pl.py
@@ -31,10 +31,27 @@ class Leaguepedia:
 
     @classmethod
     def set_query_delay(cls: Leaguepedia, delay: int) -> None:
+        """Set the query delay for the Leaguepedia API.
+
+        Args:
+            cls (Leaguepedia): The Leaguepedia class.
+            delay (int): The delay value to be set.
+
+        Returns:
+            None
+        """
         cls.DELAY = delay
 
     @classmethod
     def delay_between_query(cls: Leaguepedia) -> None:
+        """Delays the execution of queries by the specified delay time.
+
+        Args:
+            cls (Leaguepedia): The Leaguepedia class instance.
+
+        Returns:
+            None
+        """
         delay = cls.DELAY - (time.time() - cls.last_query)
 
         if delay > 0:
@@ -52,13 +69,37 @@ class Leaguepedia:
             )
         return pl.DataFrame()
 
-    def get_leagues(self: Leaguepedia, where: str = "") -> pl.DataFrame:
+    def get_leagues(
+        self: Leaguepedia,
+        *,
+        tables: list[str] | None = None,
+        join_on: str = "",
+        where: str = "",
+    ) -> pl.DataFrame:
+        """Retrieves leagues from Leaguepedia.
+
+        Args:
+            tables (Optional[list[str]]): A list of table names to query.
+                Defaults to None.
+            join_on (str): The join condition for the tables. Defaults to "".
+            where (str): The WHERE condition for the query. Defaults to "".
+
+        Returns:
+            DataFrame: A pandas DataFrame containing the retrieved leagues.
+        """
+        if tables is None:
+            tables = []
+        if any(table not in self.TABLES for table in tables):
+            msg = "Invalid table name."
+            raise ValueError(msg)
+        tables = ", ".join(self.TABLES[table] for table in {*tables, "leagues"})
         self.delay_between_query()
 
         response = self.api(
             "cargoquery",
             limit="max",
-            tables=self.TABLES["leagues"],
+            tables=tables,
+            join_on=join_on,
             fields="L.League, L.League_Short, L.Region, L.Level, L.IsOfficial",
             where=where,
         )
@@ -72,6 +113,20 @@ class Leaguepedia:
         join_on: str = "",
         where: str = "",
     ) -> pl.DataFrame:
+        """Retrieves tournaments data from Leaguepedia.
+
+        Args:
+            tables (list[str] | None, optional): A list of tables to include in the query.
+                Defaults to None.
+            join_on (str, optional): The join condition for the tables. Defaults to "".
+            where (str, optional): The where clause for the query. Defaults to "".
+
+        Returns:
+            pl.DataFrame: A DataFrame containing the retrieved tournaments data.
+
+        Raises:
+            ValueError: If an invalid table name is provided in the 'tables' parameter.
+        """
         if tables is None:
             tables = []
         if any(table not in self.TABLES for table in tables):
@@ -107,3 +162,84 @@ class Leaguepedia:
                 pl.col(datetime_cols).str.strptime(pl.Datetime, "%Y-%m-%d"),
             )
         return tournaments
+
+    def get_scoreboard_games(
+        self: Leaguepedia,
+        *,
+        tables: list[str] | None = None,
+        join_on: str = "",
+        where: str = "",
+    ) -> pl.DataFrame:
+        """Retrieves the scoreboard games from the Leaguepedia API.
+
+        Args:
+            self (Leaguepedia): The Leaguepedia instance.
+            tables (list[str] | None): A list of table names to query. Defaults to None.
+            join_on (str): The join condition for the tables. Defaults to an empty string.
+            where (str): The where condition for the query. Defaults to an empty string.
+
+        Returns:
+            pl.DataFrame: The scoreboard games data as a Pandas DataFrame.
+
+        Raises:
+            ValueError: If an invalid table name is provided.
+        """
+        if tables is None:
+            tables = []
+        if any(table not in self.TABLES for table in tables):
+            msg = "Invalid table name."
+            raise ValueError(msg)
+        tables = ", ".join(
+            self.TABLES[table] for table in {*tables, "scoreboard_games"}
+        )
+        self.delay_between_query()
+
+        response = self.api(
+            "cargoquery",
+            limit="max",
+            tables=tables,
+            join_on=join_on,
+            fields=(
+                "SG.OverviewPage, SG.Tournament, SG.Team1, SG.Team2, SG.WinTeam,"
+                " SG.LossTeam, SG.DateTime_UTC, SG.DST, SG.Team1Score, SG.Team2Score,"
+                " SG.Winner, SG.Gamelength, SG.Gamelength_Number, SG.Team1Bans,"
+                " SG.Team2Bans, SG.Team1Picks, SG.Team2Picks, SG.Team1Players,"
+                " SG.Team2Players, SG.Team1Dragons, SG.Team2Dragons, SG.Team1Barons,"
+                " SG.Team2Barons, SG.Team1Towers, SG.Team2Towers, SG.Team1Gold,"
+                " SG.Team2Gold, SG.Team1Kills, SG.Team2Kills, SG.Team1RiftHeralds,"
+                " SG.Team2RiftHeralds, SG.Team1Inhibitors, SG.Team2Inhibitors,"
+                " SG.Patch, SG.PatchSort, SG.MatchHistory, SG.VOD, SG.N_Page,"
+                " SG.N_MatchInTab, SG.N_MatchInPage, SG.N_GameInMatch, SG.Gamename,"
+                " SG.UniqueLine, SG.GameId, SG.MatchId, SG.RiotPlatformGameId,"
+                " SG.RiotPlatformId, SG.RiotGameId, SG.RiotHash, SG.RiotVersion"
+            ),
+            where=where,
+        )
+
+        scoreboard_games = self.__from_response(response)
+        if not scoreboard_games.is_empty():
+            int_cols = [
+                "Team1Score",
+                "Team2Score",
+                "Winner",
+                "Team1Dragons",
+                "Team2Dragons",
+                "Team1Barons",
+                "Team2Barons",
+                "Team1Towers",
+                "Team2Towers",
+                "Team1Kills",
+                "Team2Kills",
+                "Team1RiftHeralds",
+                "Team2RiftHeralds",
+                "Team1Inhibitors",
+                "Team2Inhibitors",
+            ]
+            float_cols = ["Gamelength Number", "Team1Gold", "Team2Gold"]
+            datetime_cols = ["DateTime UTC"]
+            scoreboard_games = scoreboard_games.with_columns(
+                pl.col(int_cols).cast(pl.Int32),
+                pl.col(float_cols).cast(pl.Float32),
+                pl.col(datetime_cols).str.strptime(pl.Datetime, "%Y-%m-%d %H:%M:%S"),
+            )
+        return scoreboard_games
--- parse_matches_polars.py
+++ parse_matches_polars.py
@@ -44,8 +44,52 @@ def parse_tournaments(start: int = 2011, end: int | None = None) -> None:
         tournaments.write_parquet(file_path)
 
 
+def parse_scoreboard_games(start: int = 2011, end: int | None = None) -> None:
+    """Parses the scoreboard games for a given range of years.
+
+    Args:
+        start (int, optional): The starting year to parse scoreboard games from.
+            Defaults to 2011.
+        end (int | None, optional): The ending year to parse scoreboard games until.
+            If None, the current year is used. Defaults to None.
+
+    Returns:
+        None
+    """
+    if end is None:
+        end = datetime.datetime.now(tz=ZoneInfo("Asia/Seoul")).year
+
+    leaguepedia = Leaguepedia()
+    for year in tqdm(range(start, end + 1), desc="Years"):
+        tournaments = pl.read_parquet(
+            f"./parquet/tournaments/{year}_tournaments.parquet",
+        )
+        scoreboard_games = pl.DataFrame()
+        exclude_pages = []
+        for (page,) in tqdm(
+            tournaments.select(pl.col("OverviewPage")).rows(),
+            desc="Tournaments",
+        ):
+            sg = leaguepedia.get_scoreboard_games(
+                tables=["tournaments"],
+                join_on="T.OverviewPage=SG.OverviewPage",
+                where=f'T.OverviewPage="{page}"',
+            )
+            if sg.is_empty():
+                exclude_pages.append(page)
+            else:
+                scoreboard_games = pl.concat([scoreboard_games, sg])
+        tournaments = tournaments.filter(~pl.col("OverviewPage").is_in(exclude_pages))
+        tournaments.write_parquet(f"./parquet/tournaments/{year}_tournaments.parquet")
+        file_path = Path(f"./parquet/scoreboard_games/{year}_scoreboard_games.parquet")
+        if not file_path.parent.is_dir():
+            file_path.parent.mkdir(parents=True)
+        scoreboard_games.write_parquet(file_path)
+
+
 def main() -> None:
     parse_tournaments(start=2023)
+    parse_scoreboard_games(start=2023)
 
 
 if __name__ == "__main__":
